a72213aeeb28bb77112721fef6d61f81eedb5075
RecreationalCoder22 <39005567+RecreationalCoder22@users.noreply.github.com>
2020-06-10 14:26:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace powerOf2
+{
+    class Program
+    {
+        /// ACCEPTED AND GOOD TIME COMPLEXITY, BEST SPACE COMPLEXITY
+        public static bool IsPowerOfTwo(int n)
+        {
+            if (n == 1)
+                return true;
+            else if (n == 0)
+                return false;
+            else if (n == 2)
+                return true;
+            else if (n % 2 == 0)
+                return IsPowerOfTwo(n / 2);
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            IsPowerOfTwo(218);
+        }
+    }
+}
